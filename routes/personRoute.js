67db5327f20099cb64cd8385f3046ad9bae2f437
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-14 12:02:44 +0300

    adds update controller to person route

--- routes/personRoute.js
+++ routes/personRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const addNewPerson = require('../controllers/addNewPerson');
 const fetchPerson = require('../controllers/fetchPerson');
+const updatePerson = require('../controllers/updatePerson');
 
 const personRoute = express.Router();
 
 personRoute.post('/', addNewPerson);
 personRoute.get('/:id', fetchPerson);
+personRoute.patch('/:id', updatePerson);
 
 module.exports = personRoute;
