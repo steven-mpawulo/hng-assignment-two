a2cb4b7a13960e6d1e29eb46e2933fde6d563c35
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-14 11:14:23 +0300

    adds person route to server.js

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const mongoose = require('mongoose');
+const personRoute = require('./routes/personRoute');
 
 const port = process.env.PORT || 5001;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({"message": "welcome to my server"});
 });
+
+app.use('/api', personRoute);
 
 mongoose.connect(process.env.DB_URL).then((value) => {
     console.log("database connected");
