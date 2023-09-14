a0072e53a96bf8003a30b3555848a6553909de8e
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-14 12:13:01 +0300

    adds delete person controller to route

--- routes/personRoute.js
+++ routes/personRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const addNewPerson = require('../controllers/addNewPerson');
 const fetchPerson = require('../controllers/fetchPerson');
 const updatePerson = require('../controllers/updatePerson');
+const deletePerson = require('../controllers/deletePerson');
 
 const personRoute = express.Router();
 
 personRoute.post('/', addNewPerson);
 personRoute.get('/:id', fetchPerson);
 personRoute.patch('/:id', updatePerson);
+personRoute.delete('/:id', deletePerson);
 
 module.exports = personRoute;
