d6dc3db6c92591e0e9857b6ee70d7edf8383169e
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-14 10:55:34 +0300

    creates schema and model for person

---
+++ models/Person.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const personSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "please provide person name"],
+    }
+}, {timeStamps: true});
+
+module.exports = mongoose.model('person', personSchema);
